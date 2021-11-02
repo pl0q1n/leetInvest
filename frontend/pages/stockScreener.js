cd9842560215af4b5184922edc30be6fde538bc2
pl0q1n <pl0q1n@gmail.com>
2021-11-02 10:25:57 +0300

    add trading view chart o stock screener page"

--- frontend/pages/stockScreener.js
+++ frontend/pages/stockScreener.js
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import React from "react"
 import styles from '../styles/Home.module.css'
 
@@ -21,13 +21,14 @@ const Search = () => {
   )
 }
 
-
 export default function StockScreener({toRender}) {
   // disable server side rendering?
   if (typeof window === 'undefined') {
     return null
   }
-  const [data, setData]=useState()
+  const [data, setData] = useState()
+  const tvRef = useRef(null)
+
 
   var toRender = ( <div className={styles.Portfolio}>
     <Search/>
@@ -41,25 +42,59 @@ export default function StockScreener({toRender}) {
       const response = await getShareInfo(query)
       console.log(response)
       setData(response)
+
+      const script = document.createElement('script');
+      script.src = 'https://s3.tradingview.com/tv.js'
+      script.async = false;
+      const symbol = `NASDAQ:${query}`
+      script.onload = () => {
+      new  window.TradingView.widget({
+        "width": 980,
+        "height": 610,
+        "symbol": symbol,
+        "interval": "D",
+        "timezone": "Etc/UTC",
+        "theme": "light",
+        "style": "1",
+        "locale": "ru",
+        "toolbar_bg": "#f1f3f6",
+        "enable_publishing": false,
+        "allow_symbol_change": true,
+        "container_id": "tradingview_95742"
+      })}
+      tvRef.current.appendChild(script)
     }
   },[query])
+
+  useEffect(() => {
+
+  },[])
+
+
 
   if (data) {
       const fundamental = data
       const fundamentals_val = Object.keys(fundamental).map((title) => <tr>{title}: {fundamental[title]}</tr>)
       const full_symbol = `NASDAQ=${query}`
       console.log(full_symbol)
-      return (
-        <div className={styles.Portfolio}>
-          <Search/>
-          <div>{fundamentals_val}</div>
+      toRender = (
+      <div className={styles.Portfolio}>
+        <Search />
+        <div>{fundamentals_val}</div>
+        <div  className="tradingview-widget-container" ref={tvRef}> 
+        <div id="tradingview_95742"></div>
         </div>
+      </div>
       )
   } else {
     toRender = (
       <div className={styles.Portfolio}>
-        <Search/>
+        <Search />
+        <div  className="tradingview-widget-container" ref={tvRef}> 
+        <div id="tradingview_95742"></div>
+        </div>
       </div>
+      
     )
 
   }
