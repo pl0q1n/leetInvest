b0ed3260d97bdb760dedd13031c81943d7500366
0xd34d10cc <0xd34d10cc@gmail.com>
2021-11-30 08:37:53 +0000

    Request portfolio on client side

--- frontend/pages/portfolio.js
+++ frontend/pages/portfolio.js
@@ -1,52 +1,56 @@
-import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 import dynamic from 'next/dynamic'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 const PlotComponent = dynamic(
   () => import('../components/plotHandler'),
   { ssr: false }
 )
 
-// posts will be populated at build time by getStaticProps()
-function Portfolio({ posts }) {
-  const [update, setUpdate] = useState({type: "add"})
+const Portfolio = () => {
+  const [portfolio, setPoftfolio] = useState(null)
+  const [update, setUpdate] = useState({ type: "add" })
 
-  console.log(posts)
+  useEffect(async () => {
+    const portfolio = await getPortfolio()
+    setPoftfolio(portfolio)
+  }, [])
 
-  if (!posts) {
-    console.log("you fucked up")
-    return
+  if (!portfolio) {
+    return <> Loading... </>
   }
 
-  const ratios = posts.ratios
+  console.log(portfolio)
+
+  const ratios = portfolio.ratios
   const ratiosValues = Object.keys(ratios).map((title) => <tr>{title}: {ratios[title]}</tr>)
 
-  const firstPosition = posts.positions[Object.keys(posts.positions)[0]]
+  const firstPosition = portfolio.positions[Object.keys(portfolio.positions)[0]]
   const valuesHeader = Object.keys(firstPosition).map((key) => <td>{key.replace('_', ' ')}</td>)
   const header = [<td>ticker</td>, <td>current price</td>].concat(valuesHeader)
 
-  const positions = Object.keys(posts.positions).map((ticker) =>
+  const positions = Object.keys(portfolio.positions).map((ticker) =>
     <tr>
       <td>{ticker}</td>
-      <td>{posts.prices[ticker]}</td>
-      <td>{posts.positions[ticker].cost_basis}</td>
-      <td>{posts.positions[ticker].count}</td>
+      <td>{portfolio.prices[ticker]}</td>
+      <td>{portfolio.positions[ticker].cost_basis}</td>
+      <td>{portfolio.positions[ticker].count}</td>
     </tr>
   )
+
   const portfolioChanger = (
-    <form onSubmit={(event)=>{
-      console.log("sent update for portfolio: ", update)
-      SendPortfolioUpdate(update)
+    <form onSubmit={(event) => {
       event.preventDefault();
+      console.log("sent update for portfolio: ", update)
+      sendPortfolioUpdate(update)
     }}>
       <div>
         <label>
           ActionType:
-          <select onChange={(event)=>{
-            setUpdate(prevState => ({...prevState, type: event.target.value}))
-              }
-            }>
+          <select onChange={(event) => {
+            setUpdate(prevState => ({ ...prevState, type: event.target.value }))
+          }
+          }>
             <option value="add">Add</option>
             <option value="remove">Remove</option>
           </select>
@@ -58,10 +62,10 @@ function Portfolio({ posts }) {
           <input
             name="price"
             type="number"
-            onChange={(event)=>{
-              setUpdate(prevState => ({...prevState, price: parseInt(event.target.value)}))
-                }
-            }/>
+            onChange={(event) => {
+              setUpdate(prevState => ({ ...prevState, price: parseInt(event.target.value) }))
+            }
+            } />
         </label>
       </div>
       <div>
@@ -70,9 +74,9 @@ function Portfolio({ posts }) {
           <input
             name="Count"
             type="number"
-            onChange={(event)=>{
-              setUpdate(prevState => ({...prevState, count: parseInt(event.target.value)}))
-                }} />
+            onChange={(event) => {
+              setUpdate(prevState => ({ ...prevState, count: parseInt(event.target.value) }))
+            }} />
         </label>
       </div>
       <div>
@@ -81,20 +85,20 @@ function Portfolio({ posts }) {
           <input
             name="Ticker"
             type="text"
-            onChange={(event)=>{
-              setUpdate(prevState => ({...prevState, ticker: event.target.value}))
-                }} />
+            onChange={(event) => {
+              setUpdate(prevState => ({ ...prevState, ticker: event.target.value }))
+            }} />
         </label>
       </div>
-        <input type="submit" value="Submit" />
-      </form>
+      <input type="submit" value="Submit" />
+    </form>
   )
 
-  const addToBalance = (b, ticker) => b + posts.prices[ticker] * posts.positions[ticker].count
-  const balance = Object.keys(posts.positions).reduce(addToBalance, 0)
+  const addToBalance = (b, ticker) => b + portfolio.prices[ticker] * portfolio.positions[ticker].count
+  const balance = Object.keys(portfolio.positions).reduce(addToBalance, 0)
   return (
     <div className={styles.Portfolio}>
-      <h1>Portfolio: {posts.name}</h1>
+      <h1>Portfolio: {portfolio.name}</h1>
       <h1>Total balance: {balance}$</h1>
       <h2>Fundamental of portfolio:</h2>
       <div>{ratiosValues}</div>
@@ -106,43 +110,30 @@ function Portfolio({ posts }) {
         </table>
       </div>
       <div>
-        <PlotComponent dcf={posts.dcf} price={balance} />
+        <PlotComponent dcf={portfolio.dcf} price={balance} />
       </div>
       <div>
-      {portfolioChanger}
+        {portfolioChanger}
       </div>
     </div>
   )
 }
 
 
-export async function SendPortfolioUpdate(body) {
+const sendPortfolioUpdate = async (body) => {
   const requestOptions = {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(body)
-    };
+  };
   const resp = await fetch('http://127.0.0.1:8080/portfolio', requestOptions)
-
   console.log("respo for update: ", resp)
 }
 
-// This function gets called at build time on server-side.
-// It won't be called on client-side, so you can even do
-// direct database queries. See the "Technical details" section.
-export async function getStaticProps() {
-  // Call an external API endpoint to get posts.
-  // You can use any data fetching library
+const getPortfolio = async () => {
   const res = await fetch('http://127.0.0.1:8080/portfolio')
-  const posts = await res.json()
-
-  // By returning { props: { posts } }, the Blog component
-  // will receive `posts` as a prop at build time
-  return {
-    props: {
-      posts,
-    },
-  }
+  const portfolio = await res.json()
+  return portfolio
 }
 
 export default Portfolio
